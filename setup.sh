eafbe84ed0d172da90154445902f9d253fb575a7
PostgreSQL administrator <postgres@LAPTOP-2ACOS295.localdomain>
2020-08-02 12:42:55 -0700

    new Database url

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ export ALGORITHMS=RS256
 export API_AUDIENCE=casting_agency
 export FLASK_APP=app.py
 export FLASK_ENV=development
-export DATABASE_URL=postgresql-elliptical-39769
+export DATABASE_URL=postgres://axhrvngbcvlvhu:786868c621a6ec24d2e7a317b0804666001586c9a3e13a6b02aab6aa7c13a6fc@ec2-34-225-82-212.compute-1.amazonaws.com:5432/d60ldojt0bfquf
+
