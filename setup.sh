9237ec9216bceb8c525f9aae6399cbb48db6d97c
PostgreSQL administrator <postgres@LAPTOP-2ACOS295.localdomain>
2020-08-01 20:14:54 -0700

    heroku files

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-
+export AUTH0_DOMAIN=jeffreyfsnd.auth0.com
+export ALGORITHMS=RS256
+export API_AUDIENCE=casting_agency
+export FLASK_APP=app.py
+export FLASK_ENV=development
+export DATABASE_URL=postgresql-elliptical-39769
